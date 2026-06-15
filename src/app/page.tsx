44e30cf9c577da871ac15b2915b1d9ba85d11206
Nmora511 <jomoraesneto@gmail.com>
2026-06-15 16:32:43 -0300

    feat: connect backend to frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@
 import { useEffect, useState } from "react";
 import SearchEntry from "@/components/searchEntry";
 import { SearchEntryData } from "@/types/apiTypes";
+import Image from "next/image";
+import SearchBar from "@/components/searchBar";
+import { AxiosResponse } from "axios";
+import api from "@/utils/api";
 
 // MOCKED DATA
 const testData: Array<SearchEntryData> = [
@@ -120,39 +124,102 @@ const testData: Array<SearchEntryData> = [
   },
 ];
 
+const testData2: Array<SearchEntryData> = [
+  {
+    lineBefore: {
+      speakers: [{ name: "Cellbit", id: "mestre" }],
+      text: "É o melhor relacionamento familiar que eu já vi.",
+    },
+    line: {
+      speakers: [{ name: "Thiago", id: "thiago_fritz" }],
+      text: "Relacionamento baseado em violência não é muito bom não meu querido, calma lá.",
+    },
+    lineAfter: {
+      speakers: [{ name: "Cristopher", id: "cristopher" }],
+      text: "Nunca bater no meu filho.",
+    },
+    moment: {
+      videoId: "23z-tCHgMGI",
+      time: 2494,
+      episodeName: "Equipe Kelvin",
+      episodeNumber: 1,
+      seasonName: "O Segredo na Floresta",
+      thumbnailUrl:
+        "https://static.wikia.nocookie.net/ordemparanormal/images/c/c5/O_Segredo_na_Floresta_Ep_1.jpg/revision/latest/scale-to-width-down/1000?cb=20230121021836&path-prefix=pt-br",
+    },
+    score: 1,
+  },
+];
 export default function Home() {
   const [searchText, setSearchText] = useState<string>("");
   const [searchEntries, setSearchEntries] = useState<Array<SearchEntryData>>(
     [],
   );
+  const [isSearchLoading, setIsSearchLoading] = useState<boolean>(false);
+
+  const startSearchQuery = () => {
+    setIsSearchLoading(true);
+    api
+      .get(`/search?query=${searchText}`)
+      .then((response: AxiosResponse<SearchEntryData[]>) => {
+        setIsSearchLoading(false);
+        setSearchEntries(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
-    // test log
-    console.log(searchText);
-    setSearchEntries(testData);
-  };
 
+    if (searchText == "") {
+      setSearchEntries([]);
+      return;
+    }
+
+    startSearchQuery();
+    // // test log
+    // if (counter % 2 == 0) {
+    //   setSearchEntries(testData2);
+    // } else {
+    //   setSearchEntries([]);
+    // }
+    // setCounter(counter + 1);
+  };
   return (
-    <main className="flex flex-col flex-1 items-center font-sans w-full h-full">
-      <section className="flex items-center justify-center gap-4 w-full h-full mt-6">
-        <input
-          className="bg-zinc-50 text-background rounded-lg w-[85%] h-10 border-yellow-700 border-[0.3rem]"
+    <main className="flex flex-col flex-1 gap-2 items-center font-sans w-full h-full">
+      <section className="flex items-center justify-center gap-4 w-[92%] h-full mt-6">
+        {/*TEMPORARY SUBSTITUTE FOR LOGO*/}
+        <div>
+          <Image
+            height={50}
+            width={50}
+            alt="mascara do desespero icon"
+            src={"/mascara_desespero.webp"}
+            loading="eager"
+          />
+        </div>
+        <SearchBar
+          value={searchText}
+          setValue={setSearchText}
           onChange={handleChange}
-          placeholder="Digite sua Pesquisa"
-          type="search"
-        ></input>
-        <button className="bg-yellow-700 p-4 font-bold rounded-xl">
+        />
+        {/*<button className="bg-yellow-700 p-4 font-bold rounded-xl">
           Pesquisar
-        </button>
+        </button>*/}
       </section>
       <div className="flex flex-col gap-3 h-full w-[92%] text-xl text-start">
         <h1 className="w-full border-b border-foreground font-bold">
           Respostas
         </h1>
-        {searchEntries.map((item, index) => (
-          <SearchEntry entry={item} key={index} />
-        ))}
+        {isSearchLoading ? (
+          <p>Loading...</p>
+        ) : (
+          searchEntries.map((item, index) => (
+            <SearchEntry entry={item} key={index} />
+          ))
+        )}
       </div>
     </main>
   );
