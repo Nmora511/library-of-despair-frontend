bb045f41acbd5e17a0bd2330ac296108ecf25334
Nmora511 <jomoraesneto@gmail.com>
2026-06-18 02:21:55 -0300

    feat: add a loading screen for youtube preview

--- src/components/youtubePreview.tsx
+++ src/components/youtubePreview.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface YoutubePreviewProps {
   videoId: string;
   timestamp: number;
@@ -7,18 +9,35 @@ export default function YoutubePreview({
   videoId,
   timestamp,
 }: YoutubePreviewProps): React.JSX.Element {
-  console.log(timestamp);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const embedUrl = `https://youtube.com/embed/${videoId}?start=${Math.trunc(timestamp / 1000)}`;
+  const [prevVideo, setPrevVideo] = useState({ videoId, timestamp });
+
+  if (videoId !== prevVideo.videoId || timestamp !== prevVideo.timestamp) {
+    setIsLoaded(false);
+    setPrevVideo({ videoId, timestamp }); // Atualiza a referência anterior
+  }
 
   return (
     <div className="flex-1 h-auto max-h-full min-h-0 max-w-full min-w-0 aspect-video bg-black rounded-xl cursor-pointer">
+      {!isLoaded && (
+        <div className="h-full w-full flex items-center justify-center rounded-xl  bg-zinc-800 z-10">
+          <div className="w-8 h-8 border-4 border-t-(--golden) border-neutral-700 rounded-full animate-spin"></div>
+        </div>
+      )}
+
       <iframe
         src={embedUrl}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
-        className="border-none w-full h-full rounded-xl"
+        onLoad={() => {
+          setIsLoaded(true);
+        }}
+        className={`border-none w-full h-full rounded-xl transition-opacity duration-300 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        }`}
       ></iframe>
     </div>
   );
