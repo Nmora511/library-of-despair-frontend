fcc08ad13727a9e12f3c15cbbc4d7e59ad77f5a6
Nmora511 <jomoraesneto@gmail.com>
2026-06-19 04:16:37 -0300

    feat: add types for json maps

---
+++ src/types/mapTypes.ts
@@ -0,0 +1,12 @@
+export type EpisodeIdMapItem = {
+  name: string;
+  seasonId: string;
+};
+
+export type SeasonIdMapItem = string;
+
+export type SpeakerIdMapItem = {
+  name: string;
+  imageUrl: string;
+  color: string;
+};
